336ccedb79046b19e63fb1aab4b125d0ce756db6
Oleg Hudak <olhudak@gmail.com>
2015-11-26 23:52:34 +0200

    As an admin I waant to be able to create, to edit, to view and delete a question

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @question = Question.new(content: 'Example')
+  end
+
+  test 'should be valid' do
+    assert @question.valid?
+  end
+
+  test 'can not be blank' do
+    @question.content = ' '
+    assert_not @question.valid?
+  end
+
 end

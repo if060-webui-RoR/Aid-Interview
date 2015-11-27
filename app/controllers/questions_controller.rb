d35e6c0f39c5a5ffe76e27a0f072c18c7e1f4afa
Orest Falchuk <falchuko@gmail.com>
2015-11-27 17:01:00 +0200

    Fix spaces

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
 	def index
-		@questions = Question.paginate(page: params[:page])
+    @questions = Question.paginate(page: params[:page])
 	end
 end

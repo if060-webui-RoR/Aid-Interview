c6c29c63ee5f34f5b7f47af714929195d471a2c5
Orest Falchuk <falchuko@gmail.com>
2015-11-27 22:39:40 +0200

    As an admin user I want to be able to see a list of questions

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   end
 
   # resources :users, only: [:show]
-
+    resources :questions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 20151125190753) do
+
+ActiveRecord::Schema.define(version: 20151127194955) do
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "content",    limit: 255
+    t.string   "answer",     limit: 255
+    t.integer  "topic_id",   limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name",             limit: 255
@@ -33,17 +42,5 @@ ActiveRecord::Schema.define(version: 20151125190753) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-end
-
-ActiveRecord::Schema.define(version: 20151127141836) do
-
-  create_table "questions", force: :cascade do |t|
-    t.string   "question",   limit: 255
-    t.string   "answer",     limit: 255
-    t.integer  "topic_id",   limit: 4
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
 
 end

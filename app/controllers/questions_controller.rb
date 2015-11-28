cde954ff2e2c1ca02bfcc3b38a9dbdac26bee328
vvs79 <valentyn-vvs@rambler.ru>
2015-11-28 17:08:10 +0200

    view a question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,18 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.paginate(page: params[:page])
   end
+
+  def show
+    @questions = Question.all
+    if params[:id].to_i <= @questions.count && params[:id].to_i > 0 
+      if current_user.admin?
+        @question = Question.find(params[:id])
+      else
+        render text: 'You are not admin' and return
+      end
+    else
+      render text: 'Question does not exist'
+    end   
+  end
+
 end

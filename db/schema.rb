cf776f6b7b9bfa1e0aa03ca2cf3f6ead2e14e616
“krisberry“ <tina.doskich@gmail.com>
2015-11-28 10:22:08 +0200

    Sign_up before rebase

--- db/schema.rb
+++ db/schema.rb
@@ -44,3 +44,4 @@ ActiveRecord::Schema.define(version: 20151127194955) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
+end

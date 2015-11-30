32022b8fec0f9043713ed49b4db58fcffef434a7
vvs79 <valentyn-vvs@rambler.ru>
2015-12-01 00:19:58 +0200

    list of topics 2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20151129161907) do
+=======
+ActiveRecord::Schema.define(version: 20151128123900) do
+>>>>>>> 13ae2cecb236cc0c31c78ccb539779c952c6ce2b
 
   create_table "questions", force: :cascade do |t|
     t.string   "content",    limit: 255

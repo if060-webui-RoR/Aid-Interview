b401fb60fdcc16c4660e133e7b76d9795f136a98
“krisberry“ <tina.doskich@gmail.com>
2015-11-30 23:29:35 +0200

    Add messages for not approved interviewers

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :first_name, :last_name, presence: true, length: { maximum: 50 }
+
+  def active_for_authentication? 
+    super && approved? 
+  end 
+
+  def inactive_message 
+    if !approved? 
+      :not_approved 
+    else 
+      super 
+    end 
+  end
+  
 end

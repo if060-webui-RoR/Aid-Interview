799c1912d14b0c1871a1109dca202e5150ec2f99
vvs79 <valentyn-vvs@rambler.ru>
2015-12-01 09:43:08 +0200

    list of topics 5.1

--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -1,15 +1,10 @@
 require 'test_helper'
 
 class TopicsControllerTest < ActionController::TestCase
-<<<<<<< HEAD
+
   test "should get index" do
     get :index
     assert_response :success
   end
 
-=======
-  # test "the truth" do
-  #   assert true
-  # end
->>>>>>> master
 end

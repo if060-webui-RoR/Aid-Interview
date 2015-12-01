7c22f41220f953a096616e4d3a295dbf2fb7290b
Oleg Hudak <olhudak@gmail.com>
2015-12-01 16:52:28 +0200

    recreate questions
    rebase
    
    rebase with master
    
    address comments
    
    add flash error
    
    remove indexes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
-	belongs_to :topic
-	validates :content, presence: true
+  belongs_to :topic
+  validates :content,  presence: true, uniqueness: { case_sensitive: false }
+  validates :topic,    presence: true
 end

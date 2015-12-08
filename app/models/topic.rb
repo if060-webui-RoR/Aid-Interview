5d787f37f96552b613bbbb101b61fdf0bbe15aa9
Orest Falchuk <falchuko@gmail.com>
2015-12-08 11:08:46 +0200

    Redesign pages
    Fix design
    
    Redesign pages and fill databse
    
    Redesign

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -5,7 +5,7 @@ class Topic < ActiveRecord::Base
 	  convert_options: {
       thumb: " -gravity center -crop '200x200+0+0'"
   },
-  :default_url => ActionController::Base.helpers.asset_path('no-image.png')
+  :default_url => ActionController::Base.helpers.asset_path('no-image.jpg')
   validates_attachment_size :image, less_than: 2.megabytes
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end

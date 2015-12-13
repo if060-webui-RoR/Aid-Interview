3b3eaf63fd8b4b103fb834ef674c239b37998632
vvs79 <valentyn-vvs@rambler.ru>
2015-12-13 14:11:54 +0200

    Page not found

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
     resources :questions
     resources :topics
   end
+
+  get '*a' => 'static_pages#error'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

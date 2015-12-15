fad775ed6b41f0b4292b1c624c72117427ce1d18
Oleg Hudak <olhudak@gmail.com>
2015-12-15 18:32:00 +0200

    corrected templates whithout admin
    correct routes
    
    primary sort
    
    sort users desc:asc
    
    remove class question
    
    sort by firstmame
    
    sorting users with bootstrap
    
    rebase sort with master
    
    remove images
    
    sort for new index users
    
    sort_link in new line

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,12 +1,11 @@
 class Admin::UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :check_admin
+  helper_method :sort_column, :sort_direction
+
   def index
-    if params[:waiting_approval]
-      @users = User.waiting_approval
-    else
-      @users = User.all
-    end
+    @users = (params[:waiting_approval] ?
+        User.waiting_approval : User).order("#{sort_column} #{sort_direction}")
   end
   
   def approve
@@ -21,6 +20,18 @@ class Admin::UsersController < ApplicationController
     def check_admin
       return true if current_user.admin?
       redirect_to authenticated_root_path, notice: 'Access Denied'
+    end
+
+  def column_names
+    ["first_name", "last_name"]
+  end
+
+    def sort_column
+      column_names.include?(params[:column]) ? params[:column] : "first_name"
+    end
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
     end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,4 @@ module ApplicationHelper
   def full_title(page_title = nil)
     [page_title, "AID-Interview"].compact.join(" | ")
   end
-end 
+end

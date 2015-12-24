952f913c32431f75fcae84429a1346d2be432f36
roman <roman0tershak@gmail.com>
2015-12-24 16:55:32 +0200

    edit style for breadcrumbs
    edit styles for breadcrumbs
    
    fixed bug
    
    add new line
    
    fix
    
    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  add_breadcrumb "Aid-Interview", :new_user_session_path
-  add_breadcrumb "admin", :admin_users_path, :if => :admin_controller?
+  add_breadcrumb "Home", :authenticated_root_path
 
   def admin_controller?
     self.class.name =~ /^Admin(::|Controller)/

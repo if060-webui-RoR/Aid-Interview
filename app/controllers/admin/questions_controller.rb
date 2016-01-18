05f155fd15dc859cf93e91ba4a0b67fe969af2f6
“krisberry“ <tina.doskich@gmail.com>
2016-01-18 20:04:14 +0200

    questions index page with angular
    Started with angular routes
    
    questions show page with angular
    
    Questions new and edit pages
    
    Add pagination

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -4,15 +4,24 @@ module Admin
   class QuestionsController < ApplicationController
     before_action :authenticate_user!
     before_action :check_admin
+    respond_to :json, :html
     add_breadcrumb "questions", :admin_questions_path
     def index
-      @questions = Question.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
+      respond_to do |format|
+        format.json do
+          page = params[:page].present? ? params[:page] : 1
+          @questions = Question.all.paginate(:page => page, :per_page => 10)
+          render :json => Paginator.pagination_attributes(@questions).merge!(:questions => @questions)
+        end
+        format.html
+      end
     end
 
     def show
       @question = Question.find(params[:id])
       @topic = @question.topic
       add_breadcrumb truncate(@question.content, length: 25), admin_question_path(@question)
+      respond_with :admin, @question
     rescue ActiveRecord::RecordNotFound
       flash[:danger] = 'Question does not exist!'
       redirect_to admin_questions_path
@@ -20,6 +29,7 @@ module Admin
 
     def new
       @question = Question.new
+      respond_with @question
       add_breadcrumb "new question", new_admin_question_path
     end
 
@@ -27,7 +37,7 @@ module Admin
       @question = Question.new(question_params)
       if @question.save
         flash[:success] = 'Question was successfully created'
-        redirect_to admin_question_path(@question)
+        respond_with :admin, @question, location: -> { admin_questions_path }
       else
         render 'new'
       end
@@ -35,6 +45,7 @@ module Admin
 
     def edit
       @question = Question.find(params[:id])
+      respond_with :admin, @question
       add_breadcrumb truncate(@question.content, length: 25), edit_admin_question_path(@question)
     rescue ActiveRecord::RecordNotFound
       flash[:danger] = 'Question does not exist!'
@@ -45,16 +56,16 @@ module Admin
       @question = Question.find(params[:id])
       if @question.update_attributes(question_params)
         flash[:success] = 'Question updated'
-        redirect_to admin_question_path
+        respond_with :admin, @question
       else
         render 'edit'
       end
     end
 
     def destroy
-      Question.find(params[:id]).destroy
+      @question = Question.find(params[:id]).destroy
       flash[:success] = 'Question deleted'
-      redirect_to :back
+      respond_with @question
     end
 
     private

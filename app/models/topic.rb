2c4fcaed28ccd709b077ba32da9413a66e62bbca
Orest Falchuk <falchuko@gmail.com>
2016-02-07 11:38:26 +0200

    fix

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,7 @@
 class Topic < ActiveRecord::Base
   has_many :questions
   validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 }
-  has_attached_file :image, styles: { thumb: "200x200#" }, convert_options: { thumb: " -gravity center -crop '200x200+0+0'" }, :bucket => 'aidinterview-assets', :default_url => ActionController::Base.helpers.asset_path('no-image.png')
+  has_attached_file :image, styles: { thumb: "200x200#" }, convert_options: { thumb: " -gravity center -crop '200x200+0+0'" }, bucket: 'aidinterview-assets', path: "/topics/:id/:filename", s3_protocol: :https, default_url: proc { ActionController::Base.helpers.asset_path('no-image.png') }
   validates_attachment_size :image, less_than: 2.megabytes
   validates_attachment_content_type :image, content_type: %r{\Aimage\/.*\Z}
 end
